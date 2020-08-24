2e11079cd46ca1e853e95a39b21d159677e74bb2
David Kalbermatten <david.kalbermatten@ost.ch>
2020-08-24 11:48:43 +0200

    Refactored build.py to reduce duplicate code

--- build.py
+++ build.py
@@ -1,24 +1,23 @@
 #!/usr/bin/env python3
 
-import subprocess, sys
+import subprocess
+import sys
 from pathlib import Path
 from types import MappingProxyType as frozen
 
 
 materials = Path('lehrmittel')
 SOLUTION_SIGNATURE = 'def::show_solutions['
+SUPPORTET_FILE_TYPES = ['pdf', 'html']
 
 
 def main(output_type):
     adoc_file_paths = list(materials.glob('**/*.adoc'))
+    check_for_file_type(output_type)
     for p in adoc_file_paths:
         sanity_check(p)
-    if output_type == 'PDF':
-        for p in adoc_file_paths:
-            make_pdfs(p)
-    elif output_type == 'HTML':
-        for p in adoc_file_paths:
-            make_htmls(p)
+    for p in adoc_file_paths:
+        make_files(p, output_type)
 
 
 def sanity_check(path):
@@ -30,75 +29,60 @@ def sanity_check(path):
             )
 
 
-def make_pdfs(adoc_file_path):
-    make_pdf_without_solutions(adoc_file_path)
+def check_for_file_type(output_type):
+    if SUPPORTET_FILE_TYPES.count(output_type.lower()) == 0:
+        raise Exception(
+            f'The File Type "{output_type}" is not supported by this Build-Script!'
+            f'The following types are supported: "{SUPPORTET_FILE_TYPES}"'
+        )
+
+
+def make_files(adoc_file_path, output_type):
+    make_files_without_solutions(adoc_file_path, output_type)
     if has_solution(adoc_file_path):
-        make_pdf_with_solutions(adoc_file_path)
+        make_files_with_solutions(adoc_file_path, output_type)
 
 
-def make_pdf_without_solutions(adoc_file_path):
+def make_files_without_solutions(adoc_file_path, output_type):
     call_asciidoctor(
         infile=adoc_file_path,
+        output_type=output_type,
     )
 
 
-def make_pdf_with_solutions(adoc_file_path):
-    outfile_name = f"{adoc_file_path.stem}_solutions.pdf"
+def make_files_with_solutions(adoc_file_path, output_type):
+    outfile_name = f"{adoc_file_path.stem}_solutions.{output_type.lower()}"
     outfile_path = adoc_file_path.parent / outfile_name
     call_asciidoctor(
         infile=adoc_file_path,
         outfile=outfile_path,
         extra_attributes=dict(show_solutions=True),
-        output_type='PDF'
+        output_type=output_type,
     )
 
-def make_htmls(adoc_file_path):
-    make_html_without_solutions(adoc_file_path)
-    if has_solution(adoc_file_path):
-        make_html_with_solutions(adoc_file_path)
-    
-def make_html_without_solutions(adoc_file_path):
-    call_asciidoctor(
-        infile=adoc_file_path,
-        output_type='HTML',
-    )
-
-def make_html_with_solutions(adoc_file_path):
-    outfile_name = f"{adoc_file_path.stem}_solutions.html"
-    outfile_path = adoc_file_path.parent / outfile_name
-    call_asciidoctor(
-        infile=adoc_file_path,
-        outfile=outfile_path,
-        extra_attributes=dict(show_solutions=True),
-        output_type='HTML'
-    )
 
 def has_solution(adoc_file_path):
     with open(adoc_file_path) as f:
         return any(SOLUTION_SIGNATURE in line for line in f)
 
 
-def call_asciidoctor(infile, outfile=None, extra_attributes=frozen({}), output_type='PDF'):
+def call_asciidoctor(infile, outfile=None, extra_attributes=frozen({}), output_type=None):
     default_attributes = frozen({
         'icons': 'font',
         'source-highlighter': 'coderay',
     })
     attributes = {**default_attributes, **extra_attributes}
+    is_pdf = ('-r', 'asciidoctor-pdf') if outfile.lower() == 'pdf' else ()
+    file_type = ('-b', output_type.lower()) if output_type else ()
     outfile_args = ('-o', outfile) if outfile else ()
-    if output_type == 'PDF':
-        command = (
-            'asciidoctor-pdf',
-            *attributes_iterable(attributes),
-            infile,
-            *outfile_args,
-        )
-    elif output_type == 'HTML':
-        command = (
-            'asciidoctor',
-            *attributes_iterable(attributes),
-            infile,
-            *outfile_args,
-        )
+    command = (
+        'asciidoctor',
+        *attributes_iterable(attributes),
+        infile,
+        *is_pdf,
+        *file_type,
+        *outfile_args,
+    )
     subprocess.run(
         command,
         check=True,
