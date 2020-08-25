37fe941b7706fe908848ff519627f98617d78314
david-kalbermatten <david.kalbermatten@hotmail.ch>
2020-08-25 09:20:25 +0000

    Host adoc as html

--- build.py
+++ build.py
@@ -1,20 +1,24 @@
 #!/usr/bin/env python3
 
 import subprocess
+import sys
 from pathlib import Path
 from types import MappingProxyType as frozen
 
 
 materials = Path('lehrmittel')
 SOLUTION_SIGNATURE = 'def::show_solutions['
+SUPPORTET_FILE_TYPES = ['pdf', 'html']
 
 
-def main():
+def main(output_type):
     adoc_file_paths = list(materials.glob('**/*.adoc'))
+    check_for_file_type(output_type)
+    print(f'The File Generator is generating {output_type.upper()}s')
     for p in adoc_file_paths:
         sanity_check(p)
     for p in adoc_file_paths:
-        make_pdfs(p)
+        make_files(p, output_type)
 
 
 def sanity_check(path):
@@ -26,25 +30,35 @@ def sanity_check(path):
             )
 
 
-def make_pdfs(adoc_file_path):
-    make_pdf_without_solutions(adoc_file_path)
+def check_for_file_type(output_type):
+    if SUPPORTET_FILE_TYPES.count(output_type.lower()) == 0:
+        raise Exception(
+            f'The File Type "{output_type}" is not supported by this Build-Script!'
+            f'The following types are supported: "{SUPPORTET_FILE_TYPES}"'
+        )
+
+
+def make_files(adoc_file_path, output_type):
+    make_file_without_solutions(adoc_file_path, output_type)
     if has_solution(adoc_file_path):
-        make_pdf_with_solutions(adoc_file_path)
+        make_file_with_solutions(adoc_file_path, output_type)
 
 
-def make_pdf_without_solutions(adoc_file_path):
+def make_file_without_solutions(adoc_file_path, output_type):
     call_asciidoctor(
         infile=adoc_file_path,
+        output_type=output_type,
     )
 
 
-def make_pdf_with_solutions(adoc_file_path):
-    outfile_name = f"{adoc_file_path.stem}_solutions.pdf"
+def make_file_with_solutions(adoc_file_path, output_type):
+    outfile_name = f"{adoc_file_path.stem}_solutions.{output_type.lower()}"
     outfile_path = adoc_file_path.parent / outfile_name
     call_asciidoctor(
         infile=adoc_file_path,
         outfile=outfile_path,
         extra_attributes=dict(show_solutions=True),
+        output_type=output_type,
     )
 
 
@@ -53,17 +67,21 @@ def has_solution(adoc_file_path):
         return any(SOLUTION_SIGNATURE in line for line in f)
 
 
-def call_asciidoctor(infile, outfile=None, extra_attributes=frozen({})):
+def call_asciidoctor(infile, outfile=None, extra_attributes=frozen({}), output_type=None):
     default_attributes = frozen({
         'icons': 'font',
         'source-highlighter': 'coderay',
     })
     attributes = {**default_attributes, **extra_attributes}
+    is_pdf = ('-r', 'asciidoctor-pdf') if output_type.lower() == 'pdf' else ()
+    file_type = ('-b', output_type.lower()) if output_type else ()
     outfile_args = ('-o', outfile) if outfile else ()
     command = (
-        'asciidoctor-pdf',
+        'asciidoctor',
         *attributes_iterable(attributes),
         infile,
+        *is_pdf,
+        *file_type,
         *outfile_args,
     )
     subprocess.run(
@@ -79,4 +97,4 @@ def attributes_iterable(attributes_dict):
 
 
 if __name__ == '__main__':
-    main()
+    main(output_type=sys.argv[1])
